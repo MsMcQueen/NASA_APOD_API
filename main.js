a2ae8a581ddad9ee8ec8bd08b04228c8d3972ce7
MsMcQueen <kik25017@gmail.com>
2024-01-14 02:01:44 -0500

    main.js

--- main.js
+++ main.js
@@ -6,15 +6,17 @@ function getFetch(){
     console.log(choice)
     
     const url = `https://api.nasa.gov/planetary/apod?
-    api_key=B5lsFVd5ynqeRJXbjeIF3uQ0tOK0qDaEFrSNH7ub&date=${choice}`
+    api_key=zU71SV2z8UAS2tpSRxtx9Ii4giGUAk6QIufK4bCn&date=${choice}`
 
     fetch(url)
         .then(res => res.json())  // parse response as JSON
         .then(data => {
             console.log(data)
+            if( data.media_type === 'image' ){
             document.querySelector('img').src = data.hdurl
-            document.querySelector('h3').innerText = data.explanation
-        })
+            }else if(data.media_type === 'video'){
+            document.querySelector('iframe').src = data.url
+        }
         .catch(err => { 
             console.log(`error ${err}`)
         });
